2b1d4a1d8869351d93a4a6c6dffa70bd869cfac3
secretrobotron <secretrobotron@gmail.com>
2015-05-12 08:49:09 -0400

    typo fix form->thanks

--- static/js/main.js
+++ static/js/main.js
@@ -7,8 +7,8 @@
   var formCheckbox = form.querySelector('input[name="optin"]');
   var formEmail = form.querySelector('input[name="email"]');
 
-  form.onsubmit = function (e) {
-    if (!formCheckbox.checked) {
+  form.onsubmit = function (e){
+    if (!formCheckbox.checked){
 
       // optin checkbox not checked
 
@@ -16,7 +16,7 @@
       return false;
     }
 
-    if (!formEmail.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i)) {
+    if (!formEmail.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i)){
 
       // bad email address
 
@@ -25,7 +25,7 @@
     }
   };
 
-  if (thanksInQuery) {
+  if (thanksInQuery){
     form.classList.add('hide');
     thanks.classList.add('show');
   }
