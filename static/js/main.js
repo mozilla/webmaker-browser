55ebd43b12c4c6bdc723d2f48fa8aa00b76b8b4c
secretrobotron <secretrobotron@gmail.com>
2015-05-12 08:50:08 -0400

    code style fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,19 @@
 (function () {
-
   var thanksInQuery = window.location.search === '?thanks';
-
   var thanks = document.querySelector('#signup #thanks');
   var form = document.querySelector('#signup form');
   var formCheckbox = form.querySelector('input[name="optin"]');
   var formEmail = form.querySelector('input[name="email"]');
 
-  form.onsubmit = function (e){
-    if (!formCheckbox.checked){
-
+  form.onsubmit = function (e) {
+    if (!formCheckbox.checked) {
       // optin checkbox not checked
 
       e.preventDefault();
       return false;
     }
 
-    if (!formEmail.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i)){
-
+    if (!formEmail.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i)) {
       // bad email address
 
       e.preventDefault();
@@ -25,7 +21,7 @@
     }
   };
 
-  if (thanksInQuery){
+  if (thanksInQuery) {
     form.classList.add('hide');
     thanks.classList.add('show');
   }
