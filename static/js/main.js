c1ab72644330b3fb5a48ba5141c459cf8029f1e5
secretrobotron <secretrobotron@gmail.com>
2015-05-13 14:05:22 -0400

    added error tooltips to signup form

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,46 @@
 (function () {
   var thanksInQuery = window.location.search === '?thanks';
+  var errorsInQuery = window.location.search.match(/([a-zA-Z]+Error)+/g);
   var thanks = document.querySelector('#signup #thanks');
   var form = document.querySelector('#signup form');
   var formCheckbox = form.querySelector('input[name="optin"]');
   var formEmail = form.querySelector('input[name="email"]');
 
-  form.onsubmit = function (e) {
-    if (!formCheckbox.checked) {
-      // optin checkbox not checked
-
-      e.preventDefault();
-      return false;
+  var validationTests = {
+    optin: function () {
+      return !formCheckbox.checked;
+    },
+    email: function () {
+      return !formEmail.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i);
     }
+  };
 
-    if (!formEmail.value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i)) {
-      // bad email address
+  function validationFilter (validationKey) {
+    return validationTests[validationKey]();
+  }
 
+  form.onsubmit = function (e) {
+    var tests = Object.keys(validationTests).filter(validationFilter);
+
+    if (tests.length > 0) {
+      form.setAttribute('data-errors', tests.join(','));
       e.preventDefault();
       return false;
     }
   };
 
   if (thanksInQuery) {
+    form.removeAttribute('data-errors');
     form.classList.add('hide');
     thanks.classList.add('show');
+  }
+
+  if (errorsInQuery) {
+    form.setAttribute('data-errors', errorsInQuery.filter(function (error) {
+      var errorType = error.match(/([a-zA-Z]+)Error/);
+      if (errorType) {
+        return errorType[1];
+      }
+    }).join(','));
   }
 })();
