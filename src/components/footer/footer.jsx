4bebb9936a804fa0e9de4fac7723a7f3d2339b99
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-07-16 15:53:58 -0400

    Adding Tools link to footer.
    Fixes https://github.com/mozilla/webmaker-core/issues/303

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,15 +5,16 @@ module.exports = React.createClass({
     return (
       <div id="footer">
         <div className="inner">
-          <h2>Share the things you love with your friends.</h2>
-          <img src="./img/mozilla.svg" width="98" height="25" alt="Mozilla" />
           <ul className="links">
             <li><a href="#/legal">Legal</a></li>
             <li><a href="#/privacy">Privacy</a></li>
             <li><a target="_blank" href="https://sendto.mozilla.org/page/contribute/join-mozilla?source=join_link">Donate</a></li>
             <li><a href="mailto:help@webmaker.org">Contact</a></li>
             <li><a target="_blank" href="https://twitter.com/Webmaker">Twitter</a></li>
+            <li><a className="tools" target="_blank" href="https://teach.mozilla.org/tools">Thimble & X-Ray Goggles</a>
+            </li>
           </ul>
+          <img src="./img/mozilla.svg" width="98" height="25" alt="Mozilla" />
         </div>
       </div>
     );
