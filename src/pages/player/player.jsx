1917b90f86606d05e858b0f78703ae2bd9da96a4
Kate Hudson <k88hudson@gmail.com>
2015-07-16 15:16:57 -0400

    Add interactive prop to PageBlock

--- src/pages/player/player.jsx
+++ src/pages/player/player.jsx
@@ -103,6 +103,7 @@ module.exports = React.createClass({
         source: page.id === this.state.sourcePageID,
         target: page.id === this.state.selectedEl && this.state.params.mode === 'link',
         transform: this.cartesian.getTransform(page.coords),
+        interactive: this.state.isPageZoomed,
         onClick: this.onPageClick.bind(this, page)
       };
       return <PageBlock {...props} />;
