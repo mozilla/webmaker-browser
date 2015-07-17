c1036bef37c0a605105a7b75695f27befaa89ba8
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-07-17 10:28:56 -0400

    Fixing reponsive issues.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -11,7 +11,7 @@ module.exports = React.createClass({
             <li><a target="_blank" href="https://sendto.mozilla.org/page/contribute/join-mozilla?source=join_link">Donate</a></li>
             <li><a href="mailto:help@webmaker.org">Contact</a></li>
             <li><a target="_blank" href="https://twitter.com/Webmaker">Twitter</a></li>
-            <li><a className="tools" target="_blank" href="https://teach.mozilla.org/tools">Thimble & X-Ray Goggles</a>
+            <li><a className="tools" target="_blank" href="http://teach.mozilla.org/tools">Thimble & X-Ray Goggles</a>
             </li>
           </ul>
           <img src="./img/mozilla.svg" width="98" height="25" alt="Mozilla" />
