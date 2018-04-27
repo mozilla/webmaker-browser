76236d7ef4525e4c8c4bc085e337c032cfcab587
Alan Mooiman <amooiman@gmail.com>
2018-04-27 17:28:31 -0400

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,9 +12,7 @@ var Footer = React.createClass({
             <li><a href="https://webmaker.org/#/legal">{messages['legal']}</a></li>
             <li><a href="https://www.mozilla.org/en-US/privacy/websites/">{messages['privacy']}</a></li>
             <li><a target="_blank" href="https://sendto.mozilla.org/page/contribute/join-mozilla?source=join_link">{messages['donate']}</a></li>
-            <li><a href="mailto:help@webmaker.org">{messages['contact']}</a></li>
-            <li><a target="_blank" href="https://twitter.com/Webmaker">{messages['twitter']}</a></li>
-            <li><a className="tools" target="_blank" href="http://teach.mozilla.org/tools">{messages['thimble_goggles']}</a>
+            <li><a target="_blank" href="https://twitter.com/mozilla">{messages['twitter']}</a></li>
             </li>
           </ul>
           <img src="./img/mozilla.svg" width="98" height="25" alt="Mozilla" />
